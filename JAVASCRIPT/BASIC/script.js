566974e7a2e82469cfb978a11b530b329baaf3da
VIJAYASEELAM <vijayaseelam2004@gmail.com>
2025-02-02 15:36:52 +0000

    commit

---
+++ JAVASCRIPT/BASIC/script.js
@@ -0,0 +1,14 @@
+alert("HELLO WORLD")
+var a = prompt("what is your name")
+console.log("iam " + a)
+var isTrue = confirm("Are u sure u want to leave this page and blast your computer")
+if(isTrue)
+{
+    console.log("Computer is blasting")
+
+}
+else
+{
+    console.log("not blasting")
+}
+console.log("your num is " + a);
