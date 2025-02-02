1271dae9e927076566379b90a5a7291a636e3132
VIJAYASEELAM <vijayaseelam2004@gmail.com>
2025-02-02 17:40:59 +0000

    commit

--- JAVASCRIPT/BASIC/script.js
+++ JAVASCRIPT/BASIC/script.js
@@ -1,4 +1,4 @@
-alert("HELLO WORLD")
+/*alert("HELLO WORLD")
 var a = prompt("what is your name")
 console.log("iam " + a)
 var isTrue = confirm("Are u sure u want to leave this page and blast your computer")
@@ -14,3 +14,16 @@ else
 document.title = "Hey iam sad"
 document.body.style.backgroundColor = "blue" //this code adds inline css in code
 console.log("your num is " + a);
+*/
+
+let o = {
+    "name" : "Vijaya",
+    "job code" : 2058
+}
+// see yaha left side jo hh unko double quotes mein 
+//rakhne ki zarurat nahi hh , it is necessary only when life side word is having space in it
+//like "job code"
+
+console.log(o)
+o.salary = 20000;
+console.log(o)
